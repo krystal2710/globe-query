ee2b08a9126092db0a54d81db4969dc8829aa162
Krystal Ly <ly_k1@denison.edu>
2024-02-28 19:50:49 -0500

    Add translate query script

---
+++ services/translate_queries.py
@@ -0,0 +1,27 @@
+def translate_text(target: str, text: str) -> dict:
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    return result
+
+def translate_data(input_lang, output_lang, data):
+    
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    translate_text("en", "안녕하세요")
