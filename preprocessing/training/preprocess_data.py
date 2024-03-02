5e6c637066bb836bcff1d95b52d82a0e7a63a913
Krystal Ly <ly_k1@denison.edu>
2024-03-02 18:04:25 -0500

    Change preprocess data mechanism

--- preprocessing/training/preprocess_data.py
+++ preprocessing/training/preprocess_data.py
@@ -12,6 +12,22 @@ from services.translate_queries import translate_all
 
 ALL_LANGUAGES = ["en", "de", "ko", "fr", "vi", "zh"]
 
+def create_context_and_query_files(contexts_filename = "data/processed/contexts.jsonl", queries_filename = "data/processed/queries.jsonl"):
+
+    with open(contexts_filename, "w") as f:
+        # Create an empty JSONL file
+        f.write("")
+
+    print("Created contexts file at {filename}".format(filename=contexts_filename))
+    
+    with open(queries_filename, "w") as f:
+        # Create an empty JSONL file
+        f.write("")
+    
+    print("Created queries file at {filename}".format(filename=queries_filename))
+
+        
+
 def convert_all_quads():
     convert_quad("data/raw/squad-train-v1.1.json", "data/processed/squad-train-v1.1.json")
     convert_quad("data/raw/korquad-train-v1.0.json", "data/processed/korquad-train-v1.0.json")
@@ -21,20 +37,23 @@ def convert_all_quads():
     convert_cmrc("data/raw/cmrc-train-v2018.json", "data/processed/cmrc-train-v2018.json")
 
 def translate_all_data():
-    translate_all("data/processed/squad-train-v1.1.json", "en", "data/processed/squad-train-translated.json", ALL_LANGUAGES[1:])
-    translate_all("data/processed/germanquad-train.json", "de", "data/processed/germanquad-train-translated.json", ALL_LANGUAGES[0] + ALL_LANGUAGES[2:])
-    translate_all("data/processed/korquad-train-v1.0.json", "ko", "data/processed/korquad-train-translated.json", ALL_LANGUAGES[0:2]+ALL_LANGUAGES[3:])
-    translate_all("data/processed/fquad-train.json", "fr", "data/processed/fquad-train-translated.json", ALL_LANGUAGES[0:3]+ALL_LANGUAGES[4:])
-    translate_all("data/processed/uitviquad-train.json", "vi", "data/processed/uitviquad-train-translated.json", ALL_LANGUAGES[0:4]+ALL_LANGUAGES[5:])
-    translate_all("data/processed/cmrc-train-v2018.json", "zh", "data/processed/cmrc-train-translated.json", ALL_LANGUAGES[0:5])
-                  
+    translate_all("data/processed/test.json", "en", "data/processed/test-translated.json", ALL_LANGUAGES[1:], "SQUAD_PROGRESS")
+    # translate_all("data/processed/germanquad-train.json", "de", "data/processed/germanquad-train-translated.json", ALL_LANGUAGES[0] + ALL_LANGUAGES[2:])
+    # translate_all("data/processed/korquad-train-v1.0.json", "ko", "data/processed/korquad-train-translated.json", ALL_LANGUAGES[0:2]+ALL_LANGUAGES[3:])
+    # translate_all("data/processed/fquad-train.json", "fr", "data/processed/fquad-train-translated.json", ALL_LANGUAGES[0:3]+ALL_LANGUAGES[4:])
+    # translate_all("data/processed/uitviquad-train.json", "vi", "data/processed/uitviquad-train-translated.json", ALL_LANGUAGES[0:4]+ALL_LANGUAGES[5:])
+    # translate_all("data/processed/cmrc-train-v2018.json", "zh", "data/processed/cmrc-train-translated.json", ALL_LANGUAGES[0:5])
+            
 def main():
+
+    #create a collection file with all contexts and a query file with all questions
+    create_context_and_query_files()
 
     #convert datasets from hierarchical model to relational model (tabular format)
     convert_all_quads()
 
     #translate all queries from one language to all other languages and save to new files
-    translate_all_data()
+    # translate_all_data()
 
 if __name__ == '__main__':
     main()
